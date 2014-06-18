a1dceba5efc0fbac7746301c387ed349e11933e6
TJ Nicolaides <tjnicolaides@gmail.com>
2014-06-18 18:36:55 -0400

    first week's homework done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var tasks = ['Take out the trash', 'Do the laundry', 'Fix the sink', 'Finish slidedecks for class', 'Relax, chill out'];
+
+
+document.write('<ul class="list-group">');
+
+for(var i = 0; i < tasks.length; i++) {
+
+    console.log(tasks[i]); 
+    document.write('<li class="list-group-item">' + tasks[i] + '</li>');
+   
+}
+
+document.write('</ul>');
