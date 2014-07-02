02682b9341388cb4f7e6f3dd284318c2b16339ed
TJ Nicolaides <tjnicolaides@gmail.com>
2014-07-02 19:00:33 -0400

    second version of HW3

--- js/app.js
+++ js/app.js
@@ -1,50 +1,42 @@
-var tasks = [
-    {/*'name': 'Take out the trash',*/ 'complete' : true}, 
-    {'name': 'Do the laundry', 'complete' : false}, 
-    {'name': 'Fix the sink', 'complete' : true}, 
-    {'name': 'Finish slidedecks for class', 'complete' : false}, 
-    {'name': 'Relax, chill out', 'complete' : true}
-];
-
-var listHTML = '<ul class="list-group">';
-    console.log(listHTML);
-
-for(var i = 0; i < tasks.length; i++) {
-    
-    try {
-        printTask(tasks[i]);
-    } catch(error) {
-        //console.log('you have an error: ' + error);
-    }
-   
-       console.log(listHTML);
-
-}
-
-listHTML += '</ul>';
-
-    console.log(listHTML);
-
-
-
-
-
-
-
-
-function printTask(taskObj) {
-    if(taskObj.name == null) {
-        throw "You should have a task name in your object!";
-    }
-    if(taskObj['complete']) { 
-        listHTML += '<li class="list-group-item">' + taskObj.name + ' <i class="glyphicon glyphicon-ok"></i></li>';
-    } else {
-        listHTML += '<li class="list-group-item">' + taskObj.name + '</li>';
-    }
-}
-
 $(document).ready(function(){
     
-    $('.panel-body').html(listHTML);
+    var tasks = [
+        {/*'name': 'Take out the trash',*/ 'complete' : true}, 
+        {'name': 'Do the laundry', 'complete' : false}, 
+        {'name': 'Fix the sink', 'complete' : true}, 
+        {'name': 'Finish slidedecks for class', 'complete' : false}, 
+        {'name': 'Relax, chill out', 'complete' : true}
+    ];
+
+    $('.panel-body').append('<ul class="list-group"></ul>');
+    
+    for(var i = 0; i < tasks.length; i++) {
+        
+        try {
+            printTask(tasks[i]);
+        } catch(error) {
+            console.log('you have an error: ' + error);
+        }
+       
+    }
+    
+    function printTask(taskObj) {
+        if(taskObj.name == null) {
+            throw "You should have a task name in your object!";
+        }
+        if(taskObj['complete']) { 
+            $('ul.list-group').append('<li class="list-group-item complete">' + taskObj.name + ' <i class="glyphicon glyphicon-ok"></i></li>');
+        } else {
+            $('ul.list-group').append('<li class="list-group-item">' + taskObj.name + '</li>');
+        }
+    }
+    
+
+
+    //$('.panel-body').html(listHTML);
     
 });
+
+function removeCompleted() {
+    $('li.complete').remove();
+}
