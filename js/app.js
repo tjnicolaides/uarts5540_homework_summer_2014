dafbfa7e593391888b2029cde08fe5c2020400c0
TJ Nicolaides <tjnicolaides@gmail.com>
2014-07-09 22:39:40 +0000

    fourth HW example

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
     
-    var tasks = [
-        {/*'name': 'Take out the trash',*/ 'complete' : true}, 
-        {'name': 'Do the laundry', 'complete' : false}, 
-        {'name': 'Fix the sink', 'complete' : true}, 
-        {'name': 'Finish slidedecks for class', 'complete' : false}, 
-        {'name': 'Relax, chill out', 'complete' : true}
-    ];
+var tasks = [];    
+    
+$(document).ready(function(){
+
+    addTask('Take out the trash');    
+    addTask('Do the laundry');    
+    addTask('Fix the sink');    
 
     $('.panel-body').append('<ul class="list-group"></ul>');
     
@@ -19,27 +18,46 @@ $(document).ready(function(){
         }
        
     }
-    
-    function printTask(taskObj) {
-        if(taskObj.name == null) {
-            throw "You should have a task name in your object!";
-        }
-        var listHTML  = $('<li>').text(taskObj.name).addClass('list-group-item');
-        
-        if(taskObj['complete']) { 
-            listHTML.addClass('complete');
-            listHTML.append(' <i class="glyphicon glyphicon-ok"></i>');
-        }
-        
-        $('ul.list-group').append(listHTML);
-    }
-    
 
-
-    //$('.panel-body').html(listHTML);
-    
 });
 
 function removeCompleted() {
     $('li.complete').remove();
+}
+
+function Task(taskname) {
+    this.name = taskname || 'test';
+    this['complete'] = false;
+    
+    var deadline = new Date();
+	deadline.setDate(deadline.getDate() + 7);
+	
+    this.dueDate = deadline;
+}
+
+function addTask(taskname) {
+    
+    // call new Task
+    var task = new Task(taskname);
+    console.log(task);
+    // add result of new Task to tasks array
+    tasks.push(task);
+    
+    // add object to HTML list
+    printTask(task);
+    
+}
+
+function printTask(taskObj) {
+    if(taskObj.name == null) {
+        throw "You should have a task name in your object!";
+    }
+    var listHTML  = $('<li>').text(taskObj.name).addClass('list-group-item');
+    
+    if(taskObj['complete']) { 
+        listHTML.addClass('complete');
+        listHTML.append(' <i class="glyphicon glyphicon-ok"></i>');
+    }
+    
+    $('ul.list-group').append(listHTML);
 }
